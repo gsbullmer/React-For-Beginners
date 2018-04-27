944accccb924e9e79d58ebed89a4d84b776df4f3
Greg Bullmer <gsbullmer@gmail.com>
2018-04-27 15:23:30 -0500

    Finish video 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,9 +11,11 @@ class App extends Component {
 
     this.state = {
       fishes: {},
+      order: {},
     };
 
     this.addFish = this.addFish.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     this.loadSampleFishes = this.loadSampleFishes.bind(this);
   }
 
@@ -24,6 +26,15 @@ class App extends Component {
     fishes[`fish${Date.now()}`] = fish;
     // 3. Set the new fishes object to state
     this.setState({ fishes });
+  }
+
+  addToOrder(key) {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
   }
 
   loadSampleFishes() {
@@ -37,7 +48,12 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,10 +3,20 @@ import { PropTypes as t } from 'prop-types';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.addToOrder(this.props.index);
+  }
+
   render() {
     const {
       name, image, desc, price, status,
     } = this.props.details;
+    const isAvailable = status === 'available';
 
     return (
       <li className="menu-fish">
@@ -15,13 +25,16 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button onClick={this.handleClick} disabled={!isAvailable}>
+          {isAvailable ? 'Add To Cart' : 'Sold Out!'}
+        </button>
       </li>
     );
   }
 }
 
 Fish.propTypes = {
+  index: t.string.isRequired,
   details: t.shape({
     name: t.string.isRequired,
     image: t.string.isRequired,
@@ -29,6 +42,7 @@ Fish.propTypes = {
     price: t.number.isRequired,
     status: t.string.isRequired,
   }).isRequired,
+  addToOrder: t.func.isRequired,
 };
 
 export default Fish;
